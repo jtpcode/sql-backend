268e46c7da15e3cb1cc9da8b66bfc177f1d9c8e8
jtpcode <152286055+jtpcode@users.noreply.github.com>
2025-10-17 11:38:21 +0300

    E13.8

---
+++ controllers/users.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt')
+const router = require('express').Router()
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll({
+    attributes: { exclude: ['passwordHash'] }
+  })
+  res.json(users)
+})
+
+router.post('/', async (req, res) => {
+  const { username, name, password } = req.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = await User.create({
+    username,
+    name,
+    passwordHash,
+  })
+
+  res.status(201).json(user)
+})
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findByPk(req.params.id, {
+    attributes: { exclude: ['passwordHash'] }
+  })
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).end()
+  }
+})
+
+// Username vaihtaminen
+router.put('/:username', async (req, res) => {
+  const { username } = req.params
+  const { newName } = req.body
+
+  const user = await User.findOne({ where: { username } })
+  if (!user) {
+    return res.status(404).end()
+  }
+
+  user.name = newName
+  await user.save()
+
+  res.json(user)
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
-// Virheenkäsittely
+// Virheenkäsittely: express versio 5+ automaattisesti käsittelee async/await virheet
 app.use((err, req, res, next) => {
   console.error(err)
   res.status(500).json({ error: err.message })
